d419388ff2443321c6f8901913156252f1884b35
Severyn Rybchynskyy <sryb@MACMINI991.local>
2015-01-29 18:57:41 +0200

    generated cat model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129141442) do
+ActiveRecord::Schema.define(version: 20150129164323) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
