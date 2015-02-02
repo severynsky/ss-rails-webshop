7c000637493af51c9659f32c3d09ec80fa2302bc
severynsky <severynr@gmail.com>
2015-02-02 16:56:12 +0200

    minor chabges

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :manufactures
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
