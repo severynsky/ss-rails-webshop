6947e94e3d0075f801f52c022d53934ee8830df3
severynsky <severynr@gmail.com>
2015-02-03 13:04:58 +0200

    added role functionality, but not roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201101828) do
+ActiveRecord::Schema.define(version: 20150203094759) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150201101828) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
