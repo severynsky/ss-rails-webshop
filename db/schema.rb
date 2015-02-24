4f73df7b7bb7825356db1886932eb9a20cdcca00
severynsky <severynr@gmail.com>
2015-02-24 11:44:54 +0200

    moino

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20150206153133) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
+
   create_table "manufactures", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",  null: false
